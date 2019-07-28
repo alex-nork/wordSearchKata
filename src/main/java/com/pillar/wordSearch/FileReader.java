571faf24e9f574779ef47ef5699497b3fc631dc1
Alex Nork <alexnork@gmail.com>
2019-07-27 20:23:31 -0400

    search is returning the intended result (search word: line number) for search word Scotty reading left to right. Thinking this main method will need to be refactored. Still need all other search directions.

--- src/main/java/com/pillar/wordSearch/FileReader.java
+++ src/main/java/com/pillar/wordSearch/FileReader.java
@@ -6,6 +6,9 @@ import java.util.Scanner;
 
 public class FileReader {
 	
+	private static SearchLogic logic = new SearchLogic();
+	
+	
 	public static void main(String[] args) throws FileNotFoundException {
 		
 		try (Scanner userInput = new Scanner(System.in)) {
@@ -27,15 +30,29 @@ public class FileReader {
 				break;
 			}
 			//this will be referenced for y-coordinate
-			int lineNumber = 1;
 			try (Scanner inputScanner = new Scanner(inputFile.getAbsoluteFile())) {
-				while(inputScanner.hasNextLine()) {
-					/*
-					 * Here is where the logic methods will be called
-					 * These will include leftToRight, rightToLeft,
-					 * topToBottom, bottomToTop, and diagonal
-					 */
+				String[] searchWords = logic.searchWordsSeparated(inputScanner.nextLine());
+				for(String word : searchWords) {
+					int lineNumber = 0;
+					Scanner newInputScanner = new Scanner(inputFile.getAbsoluteFile());
+					if(newInputScanner.hasNextLine()) {
+						newInputScanner.nextLine();
+					}
+						while(newInputScanner.hasNextLine()) {
+							String line = newInputScanner.nextLine();
+							if(logic.doesContainSearchWord(word, logic.stringOfLetters((line)))) {
+								System.out.println(word + ": " + lineNumber);
+							}
+							lineNumber++;
+							/*
+							 * Here is where the logic methods will be called
+							 * These will include leftToRight, rightToLeft,
+							 * topToBottom, bottomToTop, and diagonal
+							 */
+						}
+					newInputScanner.close();
 				}
+				
 			}
 			
 		}
